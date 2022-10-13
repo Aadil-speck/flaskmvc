fca8decb9b720c1d5b67f8be12080708c977ac63
Aadil-speck <aadiljali9@gmail.com>
2022-10-13 23:50:24 +0000

    ubdates

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,7 +1,5 @@
 from .ICT_team import *
 from .auth_ICT_team import *
-from .admin import *
-from .auth_admin import *
 from .business_User import *
 from .auth_business_User import *
 from .customer import *
